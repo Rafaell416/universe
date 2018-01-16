850ad77ccca49ca274933ff30c545132d589a29f
Rafael Villarreal <rvillarreal416@gmail.com>
2018-01-16 17:05:25 -0500

    agent send a message on connected

--- universe-mqtt/server.js
+++ universe-mqtt/server.js
@@ -6,7 +6,7 @@ const redis = require('redis')
 const chalk = require('chalk')
 const db = require('universe-db')
 const config = require('../universe-db/config-db')(false)
-
+const { parsePayload } = require('./utils')
 
 const backed = {
   type: 'redis',
@@ -20,21 +20,70 @@ const settings = {
 }
 
 const server = new mosca.Server(settings)
+const clients = new Map()
+
+let Agent, Metric
 
 server.on('clientConnected', client => {
   debug(`Client Connected: ${client.id}`)
+  clients.set(client.id, null)
 })
 
 server.on('clientDisconnected', client => {
   debug(`Client Disconnected: ${client.id}`)
 })
 
-server.on('published', (packet, client) => {
+server.on('published', async (packet, client) => {
   debug(`Received: ${packet.topic}`)
-  debug(`Payload: ${packet.payload}`)
+
+  switch (packet.topic) {
+    case 'agent/connected':
+    case 'agent/disconnected':
+      debug(`Payload: ${packet.payload}`)
+      break
+    case 'agent/message':
+      debug(`Payload: ${packet.payload}`)
+
+      const payload = parsePayload(packet.topic)
+
+      if (payload) {
+        payload.agent.connected = true
+
+        let agent
+        try {
+          agent = await Agent.createOrUpdate(payload.agent)
+        } catch (e) {
+          return handleError(e)
+        }
+        debug(`Agent: ${agent.uuid} saved`)
+
+        if (!clients.get(client.id)) {
+          clients.set(client.id, agent)
+          server.publish({
+            topic: 'agent/connected',
+            payload: JSON.stringify({
+              agent: {
+                uuid: agent.uuid,
+                name: agent.name,
+                hostname: agent.hostname,
+                pid: agent.pid,
+                connected: agent.connected
+              }
+            })
+          })
+        }
+      }
+
+      break
+  }
 })
 
-server.on('ready', () => {
+server.on('ready', async () => {
+  const services = await db(config).catch(handleFatalError)
+
+  Agent = services.Agent
+  Metric = services.Metric
+
   console.log(`${chalk.green('[universe-mqtt]')} server is running`)
 })
 
@@ -44,6 +93,11 @@ function handleFatalError (err) {
   console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
+}
+
+function handleError (err) {
+  console.error(`${chalk.yellow('[error]')} ${err.message}`)
+  console.error(err.stack)
 }
 
 process.on('uncaughtException', handleFatalError)
