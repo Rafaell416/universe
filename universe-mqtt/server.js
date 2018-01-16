faad05171c637453785b1697fdb79221c4d7a455
Rafael Villarreal <rvillarreal416@gmail.com>
2018-01-16 16:27:07 -0500

    config file

--- universe-mqtt/server.js
+++ universe-mqtt/server.js
@@ -4,3 +4,47 @@ const debug = require('debug')('universe:mqtt')
 const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
+const db = require('universe-db')
+const config = require('../universe-db/config-db')(false)
+
+
+const backed = {
+  type: 'redis',
+  redis,
+  return_buffers: true
+}
+
+const settings = {
+  port: 1883,
+  backed
+}
+
+const server = new mosca.Server(settings)
+
+server.on('clientConnected', client => {
+  debug(`Client Connected: ${client.id}`)
+})
+
+server.on('clientDisconnected', client => {
+  debug(`Client Disconnected: ${client.id}`)
+})
+
+server.on('published', (packet, client) => {
+  debug(`Received: ${packet.topic}`)
+  debug(`Payload: ${packet.payload}`)
+})
+
+server.on('ready', () => {
+  console.log(`${chalk.green('[universe-mqtt]')} server is running`)
+})
+
+server.on('error', handleFatalError)
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
